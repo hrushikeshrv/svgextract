27035a3c3ab6f8ffe08ed3335849eca03d9afaab
Hrushikesh <hrushikeshvaidya2002@gmail.com>
2021-02-17 13:28:32 +0530

    Improved the extraction algorithm. Now sets the width and height optimally

--- SVGExtractor.py
+++ SVGExtractor.py
@@ -4,34 +4,54 @@ import webbrowser
 import os
 
 class SVGExtractor():
-    def extract(self, page, file_name, open_html = True):
+    def __get_page(self, page):
         """
-        Takes in the URL to a page and extracts all the <svg> elements on that page.
-        DOES NOT WORK 100% ACCURATELY FOR ALL PAGES
-        
-        args-
-        page:str - The url to a page to extract svgs from
-        file_name:str - The file name to save the extracted svgs as (WITHOUT the extension. The script sets the extension as .html automatically)
-        open_html:bool - Opens the extracted svgs in browser if True
+        Takes in a url to a page as a string and returns the BeautifulSoup
         """
-        
-        #Get the page
-        print('Getting the page you requested')
+        print('Getting the page you requested...')
         response = requests.get(page)
-
-        #Check for errors
         response.raise_for_status()
         print('Page retrieved succesfully\n')
-
-        #Convert the response into a BeautifulSoup object
         html = BeautifulSoup(response.text, features="lxml")
-        #Select all the svg elements
-        svgs = html.select('svg')
-        #Convert each element in svgs from a bs4.element.Tag object to just a string
-        svg_strings = [str(x) for x in svgs]
-        print('SVGs extracted...\n')
+        
+        return html
+    
+    def __get_svgs(self, soup):
+        """
+        Takes in the BeautifulSoup object and returns all svg elements with their width and height info in JSON format
+        """
+        svgs = soup.select('svg')
+        svg_json = {}
+        for svg in range(len(svgs)):
+            svg_json[svg] = {}
+            svg_json[svg]['html'] = svgs[svg]
 
-        #Initialize the metadata to write to the html file
+            if 'viewbox' in svgs[svg].attrs:
+                _ = svgs[svg].attrs['viewbox']
+                if ',' in _:
+                    svg_json[svg]['width'] = _.split(',')[-2]
+                    svg_json[svg]['height'] = _.split(',')[-1]
+                else:
+                    svg_json[svg]['width'] = _.split()[-2]
+                    svg_json[svg]['height'] = _.split()[-1]
+
+            elif 'width' in svgs[svg].attrs or 'height' in svgs[svg].attrs:
+                svg_json[svg]['width'] = svgs[svg].attrs.get('width', 'auto')
+                svg_json[svg]['height'] = svgs[svg].attrs.get('height', 'auto')
+            
+            else:
+                svg_json[svg]['width'] = 'auto'
+                svg_json[svg]['height'] = 'auto'
+
+            assert svg_json[svg]['width'].isnumeric() or svg_json[svg]['width'] == 'auto'
+            assert svg_json[svg]['height'].isnumeric() or svg_json[svg]['height'] == 'auto'
+            print('SVGs extracted successfully\n')
+        return svg_json
+    
+    def __write_html(self, svg_json, file_name):
+        """
+        Takes in a file name and a JSON object of the svgs found and writes the svgs to an HTML file
+        """
         metadata = f"""
             <html>
                 <head>
@@ -43,57 +63,62 @@ class SVGExtractor():
                             display: flex;
                             flex-flow: row wrap;
                             justify-content: center;
-                            align-items: stretch;
+                            align-items: center;
                         }}
                         .svg-element {{
                             padding: 20px;
                             margin: 30px;
                             border: 3px solid rgb(252, 173, 3);
                             border-radius: 7px;
-                            display: flex;
-                            align-items: center;
-                            justify-content: center;
                         }}
                     </style>
                 </head>
                 <body>
         """
-
-        #Open a file
-        print(f'Writing to {file_name}.html')
-        with open(file_name + '.html', 'w') as file:
-            #Set the file up to be written to and add some styles
+        
+        with open(os.path.join(os.getcwd(), 'extracted', file_name), 'w') as file:
             file.write(metadata)
-            #Write all extracted svgs to the file
-            for svg in svg_strings:
-                file.write('<div class="svg-element">\n')
-                file.write(svg)
+            
+            for key in svg_json:
+                file.write(f'<div class="svg-element" style="width:{svg_json[key]["width"]};height:{svg_json[key]["height"]}">\n')
+                file.write(str(svg_json[key]['html']))
                 file.write('</div>\n')
             
-            #End the file by closing the body and html tags
             file.write('</body></html>')
-        print('HTML file created\n')
-
-        #Open the file if open_html is True
+        
+        print('HTML file written successfully\n')
+            
+        
+    def extract(self, page, file_name, open_html = True):
+        """
+        Takes in the url to a page and extracts all the <svg> elements on the page.
+        
+        args-
+        page:str - The url to a page to extract <svg> elements from
+        file_name:str - The file name to save it as (without the extension, the script sets the extension as html automatically)
+        open_html:bool - Opens the extracted svgs in a browser if True
+        """
+        html = self.__get_page(page)
+        svg_json = self.__get_svgs(html)
+        self.__write_html(svg_json, file_name)
+        
         if open_html:
-            print("Opening the created html file\n\n")
+            print('Opening the created html file')
             #Get the path
-            path = os.path.join(os.getcwd(), file_name + '.html')
-            #Open the file
+            path = os.path.join(os.getcwd(), 'extracted', file_name)
+            #Open html file in the default browser
             webbrowser.open(path)
 
 if __name__ == "__main__":
     svge = SVGExtractor()
-    print('\n\nSVGExtractor does not work 100% accurately for all pages yet.\nIf you find an issue please report it on GitHub at https://www.github.com/hrushikeshrv/svgextract\n\n')
+    print('\n\nSVGExtractor does not work 100% accurately for all pages yet, most notably for pages which animate their SVGs.\nIf you find an issue please report it on GitHub at https://www.github.com/hrushikeshrv/svgextract\n\n')
 
     continue_flag = True
     while continue_flag:
         page = input('Enter the direct url to the webpage you want to extract svgs from. - ')
         file_name = input("Enter a name to save the file as (WITHOUT the extension, the extension will be set to .html automatically) - ")
-        flag = input('Do you want to open the extracted svgs when done? Y/n - ')
-        flag = flag.lower().startswith('y')
+        flag = input('Do you want to open the extracted svgs when done? Y/n - ').lower().startswith('y')
 
         svge.extract(page, file_name + '.html', flag)
         
-        continue_flag = input("Do you want to extract again? Y/n - ")
-        continue_flag = continue_flag.lower().startswith('y')
+        continue_flag = input("Do you want to extract again? Y/n - ").lower().startswith('y')
